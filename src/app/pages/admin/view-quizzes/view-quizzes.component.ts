c3a556ea046e0e507535360dde7d9925d5b3196a
Bhargav Bhutwala <bhargav1999.bb@gmail.com>
2024-08-28 12:02:08 -0400

    Deleting Quiz | Admin Dashboard

--- src/app/pages/admin/view-quizzes/view-quizzes.component.ts
+++ src/app/pages/admin/view-quizzes/view-quizzes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from '../../../services/quiz.service';
+import Swal from 'sweetalert2';
+import { title } from 'process';
+import { text } from 'stream/consumers';
 
 @Component({
   selector: 'app-view-quizzes',
@@ -22,6 +25,45 @@ export class ViewQuizzesComponent implements OnInit{
         console.log(error);
       }
     );
+  }
+
+  deleteQuiz(qId: any){
+
+    Swal.fire({
+      icon: 'warning',
+      title: 'Are you sure?',
+      showConfirmButton: true,
+      confirmButtonColor: '#ff3f33',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((response: any) => {
+      if(response.isConfirmed){
+
+        //delete
+        this.quizService.deleteQuiz(qId).subscribe(
+          (data)=>{
+            this.quizzes = this.quizzes.filter((quiz: any) => quiz.qId !== qId);
+            Swal.fire(
+              {
+                title: 'Success!',
+                text: 'Quiz deleted successfully',
+                icon:'success'
+              }
+            );
+          },
+          (error) => {
+            console.log(error);
+            Swal.fire({
+              title: 'Error!',
+              text: 'Failed to delete quiz',
+              icon: 'error'
+            });
+          }
+        );
+      }
+    });
+
   }
 
 }
